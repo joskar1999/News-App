f7ef7a7da1057e6bf112d69e81e3f3ddac5dff19
Adam Stelmaszyk <adamstelmaszyk@MacBook-Air-Adam.local>
2018-07-11 19:29:46 +0200

    main layout edited and created listview

--- app/src/main/java/com/oskarjerzyk/newsapp/MainActivity.java
+++ app/src/main/java/com/oskarjerzyk/newsapp/MainActivity.java
@@ -1,13 +1,51 @@
 package com.oskarjerzyk.newsapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    ListView lv;
+    Context context;
+    ArrayList progList;
+
+    public List<Integer> progImages;
+
+    public List<String> progNames;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progNames = new ArrayList<String>();
+        progImages = new ArrayList<Integer>();
+
+        /*progImages.add(R.drawable.ikona);
+        progImages.add(R.drawable.zd);
+        progImages.add(R.drawable.ikona);
+        progImages.add(R.drawable.zd);
+        progImages.add(R.drawable.ikona);
+        progImages.add(R.drawable.zd);*/
+
+
+        progNames.add("Object");
+        progNames.add("Class");
+        progNames.add("Object");
+        progNames.add("Class");
+        progNames.add("Object");
+        progNames.add("Class");
+
+
+        MyListAdapter adapter = new MyListAdapter(this,progNames,progImages);
+
+        lv = (ListView) findViewById(R.id.listView2);
+
+        lv.setAdapter(adapter);
     }
 }
