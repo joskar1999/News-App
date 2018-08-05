0b03c9d88183500cd457919bce6c9341aaf3b09a
joskar1999 <joskar1999@gmail.com>
2018-08-05 23:22:34 +0200

    EditAccountActivity designed in xml, navigation is working

--- app/src/main/java/com/oskarjerzyk/newsapp/EditAccountActivity.java
+++ app/src/main/java/com/oskarjerzyk/newsapp/EditAccountActivity.java
@@ -1,12 +1,17 @@
 package com.oskarjerzyk.newsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class EditAccountActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,15 @@ public class EditAccountActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Edit Account");
+
+        saveButton = (Button) findViewById(R.id.save_button_editaccount);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent accountIntent = new Intent(EditAccountActivity.this, AccountActivity.class);
+                startActivity(accountIntent);
+            }
+        });
     }
 }
