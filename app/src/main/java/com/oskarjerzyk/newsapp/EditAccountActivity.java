26d0c1ee8d1cec184a2d7b73f1f4a7bce3b12dc3
joskar1999 <joskar1999@gmail.com>
2018-08-06 15:50:18 +0200

    Sidebar header modified, FloatingActionButton added to EditAcxcountActivity

--- app/src/main/java/com/oskarjerzyk/newsapp/EditAccountActivity.java
+++ app/src/main/java/com/oskarjerzyk/newsapp/EditAccountActivity.java
@@ -1,17 +1,20 @@
 package com.oskarjerzyk.newsapp;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class EditAccountActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     private Button saveButton;
+    private FloatingActionButton addPhotoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +27,20 @@ public class EditAccountActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Edit Account");
 
         saveButton = (Button) findViewById(R.id.save_button_editaccount);
+        addPhotoButton = (FloatingActionButton) findViewById(R.id.add_photo_button_editaccount);
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent accountIntent = new Intent(EditAccountActivity.this, AccountActivity.class);
                 startActivity(accountIntent);
+            }
+        });
+
+        addPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
             }
         });
     }
