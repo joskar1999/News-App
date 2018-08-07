57721a7dadf727109d08ed73c81272d78ec9d0a1
joskar1999 <joskar1999@gmail.com>
2018-08-07 18:00:49 +0200

    Displaying personal data in AccountActivity
    All personal data will be displayed in appropriate TextViews, image is
    still default

--- app/src/main/java/com/oskarjerzyk/newsapp/AccountActivity.java
+++ app/src/main/java/com/oskarjerzyk/newsapp/AccountActivity.java
@@ -1,34 +1,91 @@
 package com.oskarjerzyk.newsapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AccountActivity extends AppCompatActivity {
 
+    private DatabaseReference database;
+    private FirebaseAuth firebaseAuth;
+
     private Toolbar toolbar;
     private Button editProfileButton;
+    private TextView emailTextView;
+    private TextView nameTextView;
+    private TextView phoneTextView;
+    private TextView addressTextView;
+
+    private PersonalData personalData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
+        database = FirebaseDatabase.getInstance().getReference().child("Users");
+        firebaseAuth = FirebaseAuth.getInstance();
+
         toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Account");
 
+        personalData = new PersonalData();
+
         editProfileButton = (Button) findViewById(R.id.edit_profile_button_account);
+        emailTextView = (TextView) findViewById(R.id.email_textview_account);
+        nameTextView = (TextView) findViewById(R.id.name_textview_account);
+        phoneTextView = (TextView) findViewById(R.id.phone_textview_account);
+        addressTextView = (TextView) findViewById(R.id.address_textview_account);
 
         editProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent editProfileIntent = new Intent(AccountActivity.this, EditAccountActivity.class);
                 startActivity(editProfileIntent);
+            }
+        });
+    }
+
+    /**
+     * Personal data will be downloaded
+     * from Firebase and set to appropriate
+     * TextViews
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        String UID = firebaseAuth.getUid().toString();
+        DatabaseReference personalDataDatabase = database.child(UID).child("personal-data");
+
+        personalDataDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                personalData = dataSnapshot.getValue(PersonalData.class);
+
+                emailTextView.setText(personalData.getEmail());
+                nameTextView.setText(personalData.getForename() + " " + personalData.getName());
+                phoneTextView.setText(personalData.getPhone());
+                addressTextView.setText(personalData.getAddress());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
---
+++ app/src/main/java/com/oskarjerzyk/newsapp/PersonalData.java
@@ -0,0 +1,54 @@
+package com.oskarjerzyk.newsapp;
+
+public class PersonalData {
+
+    private String email;
+    private String forename;
+    private String name;
+    private String phone;
+    private String address;
+
+    public PersonalData() {
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
